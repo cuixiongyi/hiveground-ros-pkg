e7e672d58b6529cd28ddf9775d472383d99365ea
Mahisorn <Mahisorn@localhost>
2012-03-28 15:20:24 +0900

    added simple client-server teleop via TCP/IP

--- robotic_arm/ve026a/ve026a_teleop/src/ve026a_teleop.cpp
+++ robotic_arm/ve026a/ve026a_teleop/src/ve026a_teleop.cpp
@@ -14,6 +14,7 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
@@ -23,7 +24,7 @@
 
 using boost::asio::ip::tcp;
 
-const int PUBLISH_FREQ = 20;
+const int PUBLISH_FREQ = 50;
 const int NUM_JOINTS = 7;
 
 using namespace std;
@@ -41,20 +42,30 @@ public:
 	ros::NodeHandle node_handle_, node_handle_private_;
 	ros::Subscriber joy_sub_;
 	ros::Subscriber joy_state_;
+	ros::Subscriber joint_state_;
 	ros::Publisher joint_pubs_[NUM_JOINTS];
 	float joint_angles_[NUM_JOINTS];
 	float joint_limits_up_[NUM_JOINTS];
 	float joint_limits_down_[NUM_JOINTS];
 	float joy_increase_step_;
+	bool joint_updated_;
 
 	tcp::socket socket_;
 	tcp::acceptor acceptor_;
-	//boost::asio::io_service& io_;
+
+	/// Strand to ensure the connection's handlers are not called concurrently.
+	//boost::asio::io_service::strand strand_;
+
+	/// Buffer for incoming data.
+	boost::array<char, 128> buffer_;
+
 
 	Ve026aTeleop(boost::asio::io_service& io)
 		:  node_handle_private_("~"),
+		   joint_updated_(false),
 		   socket_(io),
 		   acceptor_(io, tcp::endpoint(tcp::v4(), 9559))
+		   //strand_(io)
 	{
 		start_accept();
 	}
@@ -77,7 +88,6 @@ public:
 		  //new_connection->start();
 		  //start_accept();
 			std::string message = make_daytime_string();
-
 			ROS_INFO("Client connected");
 			boost::asio::async_write(socket_, boost::asio::buffer(message),
 				boost::bind(&Ve026aTeleop::handle_write, this,
@@ -93,7 +103,15 @@ public:
 
 	void handle_read(const boost::system::error_code& error, size_t size)
 	{
-		ROS_INFO("Write to client");
+		ROS_INFO("read requested");
+		if(!error)
+		{
+			ROS_INFO("get %d bytes", size);
+		}
+		else
+		{
+			ROS_INFO("read_error", size);
+		}
 	}
 
 
@@ -101,36 +119,56 @@ public:
 	{
 		if(socket_.is_open())
 		{
+				/*
+				socket_.async_read_some(boost::asio::buffer(buffer_),
+							boost::bind(&Ve026aTeleop::handle_read, this,
+							boost::asio::placeholders::error,
+							boost::asio::placeholders::bytes_transferred));
+				*/
+
 			if(socket_.available())
 			{
-
-
-
 				boost::array<char, 128> buf;
 				boost::system::error_code error;
 
 				size_t len = socket_.read_some(boost::asio::buffer(buf), error);
 
+				ROS_INFO("Get %d bytes", len);
+
 				if (error == boost::asio::error::eof)
 				{
 					ROS_INFO("Connection closed");
+					socket_.close();
+					start_accept();
 					//break; // Connection closed cleanly by peer.
 				} else if (error)
 					throw boost::system::system_error(error); // Some other error.
 
-				std::cout.write(buf.data(), len);
+				//std::cout.write(buf.data(), len);
+				if(len == 62)
+				{
+					ROS_INFO("get: %s", buf.data());
+
+					std::stringstream ss;
+					ss << buf.data();
+					for(int i = 0; i < NUM_JOINTS ; i++)
+					{
+						ss >> joint_angles_[i];
+					}
+				}
 			}
-		}
-		else
-		{
-			ROS_INFO("Not open");
 		}
 	}
 
 	void init()
 	{
 		joy_sub_ = node_handle_.subscribe("joy",
-				10, &Ve026aTeleop::joy_callback, this);
+				1, &Ve026aTeleop::joy_callback, this);
+
+
+		joint_state_ = node_handle_.subscribe("joint_states",
+						10, &Ve026aTeleop::joint_callback, this);
+
 		for(int i = 0; i < NUM_JOINTS ; i++)
 		{
 			std::stringstream ss;
@@ -169,10 +207,26 @@ public:
 
 		//J7
 		joint_limits_up_[6] = 0.43633;
-		joint_limits_down_[6] = -1.30899;
+		joint_limits_down_[6] = -0.63633;
 
-		//read joint state
 
+
+
+	}
+
+	void joint_callback(const sensor_msgs::JointStateConstPtr& joint_msg)
+	{
+		if(!joint_updated_)
+		{
+			ROS_INFO("Joint state!! %d", joint_msg->name.size());
+			for(int i = 0; i < joint_msg->name.size(); i++)
+			{
+				ROS_INFO_STREAM(joint_msg->name[i] << " " << joint_msg->position[i]);
+				joint_angles_[i] = joint_msg->position[i];
+			}
+			joint_updated_ = true;
+			joint_state_.shutdown();
+		}
 	}
 
 	void joy_callback(const sensor_msgs::Joy::ConstPtr& joy_msg)
@@ -220,6 +274,14 @@ public:
 		if(buttons[11]) joint_angles_[6] -= joy_increase_step_;
 
 
+		if(buttons[8])
+		{
+			for(int i = 0; i < NUM_JOINTS; i++)
+			{
+				joint_angles_[i] = 0;
+			}
+		}
+
 
 		/*
 		for(int i = 0; i < joy_msg->buttons.size(); i++)
@@ -257,19 +319,20 @@ public:
 		}
 		std::cout << endl;
 		*/
-		/*
+
 		for(int i = 0; i < NUM_JOINTS; i++)
 		{
 			std::cout << joint_angles_[i] << " ";
 		}
 		std::cout << endl;
-		*/
-		//publish();
+
+		publish();
 	}
 
 
 	void publish()
 	{
+		if(!joint_updated_) return;
 		for(int i = 0; i < NUM_JOINTS; i++)
 		{
 			if(joint_angles_[i] > joint_limits_up_[i])
@@ -303,11 +366,7 @@ int main(int argc, char** argv)
 	while(ros::ok())
 	{
 		ros::spinOnce();
-
-
-
 		ve026a_teleop.read_socket();
-		ve026a_teleop.publish();
 		rate.sleep();
 	}
 
