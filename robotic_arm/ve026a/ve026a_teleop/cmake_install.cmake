935bc4f783c87126c854184d22510dfd2f124b01
Mahisorn <Mahisorn@localhost>
2012-05-21 10:24:59 +0900

    updated ve026a_teleop.cpp  - check only first 7 joints updated agb65_network.launch  - remove joystick requirement

--- robotic_arm/ve026a/ve026a_teleop/cmake_install.cmake
+++ robotic_arm/ve026a/ve026a_teleop/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mahisorn/ros_workspace/hg-ros-pkg/robotic_arm/ve026a/ve026a_teleop
+# Install script for directory: /home/rosuser/ros_workspace/hg-ros-pkg/robotic_arm/ve026a/ve026a_teleop
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/mahisorn/ros_workspace/hg-ros-pkg/robotic_arm/ve026a/ve026a_teleop/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/rosuser/ros_workspace/hg-ros-pkg/robotic_arm/ve026a/ve026a_teleop/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/mahisorn/ros_workspace/hg-ros-pkg/robotic_arm/ve026a/ve026a_teleop/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/rosuser/ros_workspace/hg-ros-pkg/robotic_arm/ve026a/ve026a_teleop/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
