85f20c0778883068202722acd3389e9cb4652d6f
Mahisorn <Mahisorn@localhost>
2012-08-24 19:52:57 +0900

    added ROS+Qt template

--- les/src/les.cpp
+++ les/src/les.cpp
@@ -1,4 +1,5 @@
 #include <les/les.h>
+#include <qevent.h>
 
 LES::LES(ros::NodeHandle& nh, QWidget *parent, Qt::WFlags flags)
   : nh_(nh),
@@ -10,4 +11,10 @@ LES::LES(ros::NodeHandle& nh, QWidget *parent, Qt::WFlags flags)
 LES::~LES()
 {
 
+}
+
+void LES::closeEvent(QCloseEvent *event)
+{
+  quit_threads_ = true;
+  event->accept();
 }
