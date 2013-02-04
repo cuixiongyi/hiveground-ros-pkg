3cf9a27867cb4f73507918e9f275e27306d6a621
Mahisorn <Mahisorn@localhost>
2013-02-04 21:56:38 +0900

    Added Kalman filter for hand position

--- hiveground/hg_apps/hg_hand_interaction/include/hg_hand_interaction/hand_interaction.h
+++ hiveground/hg_apps/hg_hand_interaction/include/hg_hand_interaction/hand_interaction.h
@@ -11,13 +11,55 @@
 #include <hg_hand_interaction/HandGesture.h>
 #include <hg_object_tracking/Hands.h>
 #include <hg_hand_interaction/gesture_hand_sweep.h>
+#include <opencv2/opencv.hpp>
 
 namespace hg_hand_interaction
 {
 
+class KalmanTraker3d
+{
+public:
+  enum
+  {
+    ERROR = -1,
+    START = 0,
+    TRACK,
+    LOST,
+    DIE
+  };
+
+  enum
+  {
+    MESUREMENT = 0,
+    PREDICTED,
+    ESTIMATED
+  };
+
+
+  KalmanTraker3d();
+  ~KalmanTraker3d();
+
+  void initialize(double dt,
+                    const cv::Point3f& point,
+                    double process_noise = 1e-4,
+                    double measurement_noise = 1e-1,
+                    double error_cov = 1e-1);
+  void predict(cv::Mat& result);
+  void update(const cv::Point3f& measurement, cv::Mat& result);
+  int updateState();
+  int getState() { return current_state_; }
+
+protected:
+  cv::KalmanFilter filter_;
+  int predict_count_;
+  int update_count_;
+  int current_state_;
+};
+
 class HandInteraction : public QMainWindow
 {
   Q_OBJECT
+  static const int HAND_HISTORY_SIZE = 30;
 public:
 
   HandInteraction(QWidget *parent = 0, Qt::WFlags flags = 0);
@@ -38,10 +80,9 @@ public:
   ros::Publisher hand_gestures_publisher_;
   ros::Publisher marker_array_publisher_;
 
+  std::vector<std::vector<std::list<geometry_msgs::Point> > > hand_history_;
+  std::vector<KalmanTraker3d> hand_trackers_;
   std::vector<GestureDetector*> gesture_detectors_;
-
-
-
 };
 
 }
